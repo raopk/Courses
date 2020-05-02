dbc62915d4ffcf93d7c4f471797ff5bfc15017e9
rayopk <38057646+RAYOPOKU@users.noreply.github.com>
2020-05-01 23:59:58 -0500

    Add files via upload

---
+++ Harvard-CS50x/pset6/readability/readability.py
@@ -0,0 +1,40 @@
+from cs50 import get_string
+
+#request user input
+text = get_string("Text: ")
+
+letters = 0
+words = 0
+sentences = 0
+counter = 0
+
+for i in text:
+    counter += 1
+
+for i in range(counter):
+    # letter counts
+    if (ord(text[i]) >= 65 and ord(text[i]) <= 122):
+        letters += 1
+
+    elif (ord(text[i]) == 32 and (ord(text[i - 1]) != 33 and ord(text[i - 1]) != 46 and ord(text[i - 1]) != 63)):
+        words += 1
+
+    # sentence counts
+    elif (ord(text[i]) == 33 or ord(text[i]) == 46 or ord(text[i]) == 63):
+        sentences += 1
+        words += 1
+
+L = letters * 100 / words
+S = sentences * 100 / words
+# Coleman-Liau index
+index = round(0.0588 * L - 0.296 * S - 15.8)
+
+if (index < 1):
+    print("Before Grade 1")
+
+elif (index >= 16):
+    print("Grade 16+")
+
+else:
+    print(f"Grade {index}")
+
